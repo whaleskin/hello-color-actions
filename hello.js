0cd01fbda6262589637a8e8d74e545cfec053604
whaleskin <rickycast@gmail.com>
2024-08-17 15:28:12 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,5 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+//const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Spain!";
 console.log(hello());
 exports.hello = hello;
 
