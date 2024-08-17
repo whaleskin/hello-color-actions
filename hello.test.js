289d35c5f9fe4065ed651692c533856251a5ad32
whaleskin <rickycast@gmail.com>
2024-08-17 15:34:23 -0400

    Update hello.test.js

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,7 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    //expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual("Hello World from Spain!");
   });
 });
